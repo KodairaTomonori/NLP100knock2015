7cd404efbc6ad8280a4d6634e3db6f3b07efdb29
KODAIRATOMONORI <kodairatomonori@GN.local>
2016-04-30 13:16:21 +0900

    Remove Files

--- Python/05set/part49.py
+++ Python/05set/part49.py
@@ -3,6 +3,8 @@
 # 名詞を含む文節が動詞を含む文節にかかるものを抽出
 
 from part41 import *
+
+
 def root_word( sentence_chunk_list, dst):
      word_dst = dst
      prev_dst = int(dst)
@@ -12,7 +14,33 @@ def root_word( sentence_chunk_list, dst):
          word_dst = sentence_chunk_list[word_dst].dst
      return sentence_chunk_list[prev_dst].get_phrase()
 
-def print_noun_to_root(sentence_chunk_list, noun_phrase,  dst):
+def print_noun_to_root(sentence_chunk_list, noun_num,  other_noun_num):
+    match_flag = False
+    particle = '助詞'; noun = '名詞'
+    minimum_pass = ''
+    #phrase_chunk = sentence_chunk_list[noun_num]
+    #minimum_pass = chunk.phrase_replace(noun, 'X')
+    #phrase_chunk = sentence_chunk_list[phrase_chunk.dst]
+    tmp_dst = sentence_chunk_list[noun_num].dst
+    next_dst = lambda x: sentence_chunk_list[x].dst
+    while 1:
+        if tmp_dst == other_noun_num:
+            minimum_pass += get_'|\tY'
+            break
+        elif tmp_dst == '-1':
+            minimum_pass += get_noun2root_pass(sentence_chunk_list, noun_num, '-1') + '|'\
+                + get_noun2root_pass(sentence_chunk_list, other_noun_num, '-1') + '|'+ root_word(sentence_chunk_list, noun_num)
+            break
+        else:
+            tmp_dst = next_dst(tmp_dst)
+           
+
+    print minimum_pass
+
+def get_noun2root_pass(sentence_list, start_num):
+    
+
+ '''   
     next_dst = sentence_chunk_list[int(dst)].dst  
     print noun_phrase,
     
@@ -23,7 +51,7 @@ def print_noun_to_root(sentence_chunk_list, noun_phrase,  dst):
         dst = sentence_chunk_list[int(dst)].dst
         next_dst = sentence_chunk_list[int(dst)].dst
         #print dst, next_dst
-
+'''
 
 def make_phrase_list(sentence_chunk_list, noun_phrase,  dst):
     phrase_list = list()
@@ -32,45 +60,26 @@ def make_phrase_list(sentence_chunk_list, noun_phrase,  dst):
         phrase_list.append(sentence_chunk_list[int(dst)].get_phrase() )
         dst = sentence_chunk_list[int(dst)].dst
     return phrase_list
-
+'''
 def check_same_phrase(phrase_list1, phrase_list2):
     for phrase in phrase_list1:
         if phrase in phrase_list2:
             return True, phrase
 
     return False, 'NULL'
-
+'''
 
 
 def make_minimum_pass(sentence_chunk_list, noun_num_list):
-    particle = '助詞'; noun = '名詞'
-    match_flag = False
     count = 0
     sub_noun_num_list = list(noun_num_list)
-    for phrase_num in noun_num_list:
+    for noun_chunk_num in noun_num_list:
         count += 1
-        sub_noun_num_list.remove(phrase_num)
-        chunk = sentence_chunk_list[phrase_num]
-        first_word =  chunk.phrase_replace(noun, 'X')
-        top_phrase_list = make_phrase_list(sentence_chunk_list, \
-            chunk.get_phrase(), chunk.dst)
-        for other_num in sub_noun_num_list:
-            next_chunk = sentence_chunk_list[other_num]
-            next_phrase_list = make_phrase_list(sentence_chunk_list, \
-                next_chunk.get_phrase(), next_chunk.dst)
-            match_flag, match_num = check_same_phrase(top_phrase_list, \
-                    next_phrase_list[:-1])
-            print first_word,
-            if match_flag:
-                print ' -> ' + ' -> '.join(next_phrase_list[:-1]) +  ' -> Y'
-            else:
-                print ' | ',
-                print_noun_to_root(sentence_chunk_list, \
-                        next_chunk.phrase_replace(noun, 'Y'), next_chunk.dst)
-                if not next_chunk.get_phrase() == root_word(sentence_chunk_list, chunk.dst):
-                    print ' | ' + root_word(sentence_chunk_list, chunk.dst)
-        print ''
-        
+        for other_noun_chunk_num in non_num_list[count:]:
+            print_noun_to_root(sentence_chunk_list, noun_chunk_num, other_noun_chunk_num)
+
+            
+            
 
 def make_noun_num_list(sentence_chunk_list):
     noun_num_list = list()
@@ -83,7 +92,6 @@ def make_noun_num_list(sentence_chunk_list):
 def get_trace_pass(text_chunk_list):
     for sentence_chunk_list in text_chunk_list:
         check_print = False
-
         noun_num_list = make_noun_num_list(sentence_chunk_list)
         if noun_num_list != []:
             make_minimum_pass(sentence_chunk_list, noun_num_list)
